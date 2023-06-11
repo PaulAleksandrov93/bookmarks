665abd8b898b04b74637a2cf8031d12689e4de47
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-06-11 22:06:56 +0500

    I added the registration functionality and fixed some bugs in the URL templates

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -14,4 +14,6 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
+    path('', include('django.contrib.auth.urls')),
 ]
