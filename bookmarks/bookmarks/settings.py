547fd360fdc873a1d24c6cb152d53b22a5b73e49
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-06-12 21:05:26 +0500

    Added a model for user profiles

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for bookmarks project.
 
@@ -126,3 +127,5 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
