deb900cef04d9f06e3a6205f5e625b413b4b2959
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-06-13 19:29:27 +0500

    Added the ability to edit user profiles, created corresponding forms.

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm, UserRegistrationForm
+from .models import Profile
 
 
 def user_login(request):
@@ -38,6 +39,8 @@ def register(request):
             new_user = user_form.save(commit=False)
             # Задаем пользователю зашифрованный пароль.
             new_user.set_password(user_form.cleaned_data['password'])
+            # Создание профиля пользователя.
+            Profile.objects.create(user=new_user)
             # Сохраняем пользователя в базе данных.
             new_user.save()
             return render(request, 'account/register_done.html', {'new_user': new_user})
